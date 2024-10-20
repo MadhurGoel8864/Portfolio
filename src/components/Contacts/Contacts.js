0abe6f073c4aa65a3e811275b6fe988a3a162b52
MadhurGoel8864 <97668013+MadhurGoel8864@users.noreply.github.com>
2024-10-20 21:12:41 +0530

    Update Contacts.js

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -4,22 +4,11 @@ import CloseIcon from '@material-ui/icons/Close';
 import axios from 'axios';
 import isEmail from 'validator/lib/isEmail';
 import { makeStyles } from '@material-ui/core/styles';
+import { SiLeetcode,SiGeeksforgeeks,SiCodechef  } from "react-icons/si";
 import {
-    FaTwitter,
     FaLinkedinIn,
-    FaGithub,
-    FaYoutube,
-    FaBloggerB,
-    FaRedditAlien,
-    FaStackOverflow,
-    FaCodepen,
-    FaInstagram,
-    FaGitlab,
-    FaMediumM,
+    FaGithub
 } from 'react-icons/fa';
-
-import { SiLeetcode, SiGeeksforgeeks, SiCodechef } from "react-icons/si";
-
 import { AiOutlineSend, AiOutlineCheckCircle } from 'react-icons/ai';
 import { FiPhone, FiAtSign } from 'react-icons/fi';
 import { HiOutlineLocationMarker } from 'react-icons/hi';
@@ -314,18 +303,9 @@ function Contacts() {
                                 {contactsData.address}
                             </p>
                         </div>
-
+{/*  */}
                         <div className='socialmedia-icons'>
-                            {socialsData.leetcode && (
-                                <a
-                                    href={socialsData.leetcode}
-                                    target='_blank'
-                                    rel='noreferrer'
-                                    className={classes.socialIcon}
-                                >
-                                    <SiLeetcode aria-label='leetcode' />
-                                </a>
-                            )}
+                            
                             {socialsData.github && (
                                 <a
                                     href={socialsData.github}
@@ -346,6 +326,17 @@ function Contacts() {
                                     <FaLinkedinIn aria-label='LinkedIn' />
                                 </a>
                             )}
+                            {socialsData.leetcode && (
+                                <a
+                                    href={socialsData.leetcode}
+                                    target='_blank'
+                                    rel='noreferrer'
+                                    className={classes.socialIcon}
+                                >
+                                    {/* <FaTwitter aria-label='Twitter' /> */}
+                                    <SiLeetcode aria-label='LeetCode'/>
+                                </a>
+                            )}
                             {socialsData.geeksforgeeks && (
                                 <a
                                     href={socialsData.geeksforgeeks}
@@ -353,7 +344,8 @@ function Contacts() {
                                     rel='noreferrer'
                                     className={classes.socialIcon}
                                 >
-                                    <SiGeeksforgeeks aria-label='geeksforgeeks' />
+                                    {/* <FaTwitter aria-label='Twitter' /> */}
+                                    <SiGeeksforgeeks aria-label='geeksforgeeks'/>
                                 </a>
                             )}
                             {socialsData.codechef && (
@@ -363,69 +355,90 @@ function Contacts() {
                                     rel='noreferrer'
                                     className={classes.socialIcon}
                                 >
-                                    <SiCodechef aria-label='codechef' />
+                                    {/* <FaTwitter aria-label='Twitter' /> */}
+                                    <SiCodechef aria-label='codechef'/>
                                 </a>
                             )}
-                            // {socialsData.blogger && (
-                            //     <a
-                            //         href={socialsData.blogger}
-                            //         target='_blank'
-                            //         rel='noreferrer'
-                            //         className={classes.socialIcon}
-                            //     >
-                            //         <FaBloggerB aria-label='Blogger' />
-                            //     </a>
-                            // )}
-                            // {socialsData.youtube && (
-                            //     <a
-                            //         href={socialsData.youtube}
-                            //         target='_blank'
-                            //         rel='noreferrer'
-                            //         className={classes.socialIcon}
-                            //     >
-                            //         <FaYoutube aria-label='YouTube' />
-                            //     </a>
-                            // )}
-                            // {socialsData.reddit && (
-                            //     <a
-                            //         href={socialsData.reddit}
-                            //         target='_blank'
-                            //         rel='noreferrer'
-                            //         className={classes.socialIcon}
-                            //     >
-                            //         <FaRedditAlien aria-label='Reddit' />
-                            //     </a>
-                            // )}
-                            // {socialsData.stackOverflow && (
-                            //     <a
-                            //         href={socialsData.stackOverflow}
-                            //         target='_blank'
-                            //         rel='noreferrer'
-                            //         className={classes.socialIcon}
-                            //     >
-                            //         <FaStackOverflow aria-label='Stack Overflow' />
-                            //     </a>
-                            // )}
-                            // {socialsData.codepen && (
-                            //     <a
-                            //         href={socialsData.codepen}
-                            //         target='_blank'
-                            //         rel='noreferrer'
-                            //         className={classes.socialIcon}
-                            //     >
-                            //         <FaCodepen aria-label='CodePen' />
-                            //     </a>
-                            // )}
-                            // {socialsData.gitlab && (
-                            //     <a
-                            //         href={socialsData.gitlab}
-                            //         target='_blank'
-                            //         rel='noreferrer'
-                            //         className={classes.socialIcon}
-                            //     >
-                            //         <FaGitlab aria-label='GitLab' />
-                            //     </a>
-                            // )}
+                            {/* { {socialsData.instagram && (
+                                <a
+                                    href={socialsData.instagram}
+                                    target='_blank'
+                                    rel='noreferrer'
+                                    className={classes.socialIcon}
+                                >
+                                    <FaInstagram aria-label='Instagram' />
+                                </a>
+                            )} } */}
+                            {/* {socialsData.medium && (
+                                <a
+                                    href={socialsData.medium}
+                                    target='_blank'
+                                    rel='noreferrer'
+                                    className={classes.socialIcon}
+                                >
+                                    <FaMediumM aria-label='Medium' />
+                                </a>
+                            )} */}
+                            {/* {socialsData.blogger && (
+                                <a
+                                    href={socialsData.blogger}
+                                    target='_blank'
+                                    rel='noreferrer'
+                                    className={classes.socialIcon}
+                                >
+                                    <FaBloggerB aria-label='Blogger' />
+                                </a>
+                            )}
+                            {socialsData.youtube && (
+                                <a
+                                    href={socialsData.youtube}
+                                    target='_blank'
+                                    rel='noreferrer'
+                                    className={classes.socialIcon}
+                                >
+                                    <FaYoutube aria-label='YouTube' />
+                                </a>
+                            )}
+                            {socialsData.reddit && (
+                                <a
+                                    href={socialsData.reddit}
+                                    target='_blank'
+                                    rel='noreferrer'
+                                    className={classes.socialIcon}
+                                >
+                                    <FaRedditAlien aria-label='Reddit' />
+                                </a>
+                            )}
+                            {socialsData.stackOverflow && (
+                                <a
+                                    href={socialsData.stackOverflow}
+                                    target='_blank'
+                                    rel='noreferrer'
+                                    className={classes.socialIcon}
+                                >
+                                    <FaStackOverflow aria-label='Stack Overflow' />
+                                </a>
+                            )}
+                            {socialsData.codepen && (
+                                <a
+                                    href={socialsData.codepen}
+                                    target='_blank'
+                                    rel='noreferrer'
+                                    className={classes.socialIcon}
+                                >
+                                    <FaCodepen aria-label='CodePen' />
+                                </a>
+                            )}
+                            {socialsData.gitlab && (
+                                <a
+                                    href={socialsData.gitlab}
+                                    target='_blank'
+                                    rel='noreferrer'
+                                    className={classes.socialIcon}
+                                >
+                                    <FaGitlab aria-label='GitLab' />
+                                </a>
+                            )} */}
                         </div>
                     </div>
                 </div>
